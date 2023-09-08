b9eab114c43155d44e43aeee29bd3b2e49c72272
s0p05qj <sathishkumar.ph@walmart.com>
2023-09-08 11:46:54 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+/**
+ * Main class
+ */
+public class Hello {
+    
+	/**
+     * Main method
+     * @param args 
+     */
+    public static void main(String[] args) {
+	       System.out.println("welcome to jenkins");
+    }
+    
+   
